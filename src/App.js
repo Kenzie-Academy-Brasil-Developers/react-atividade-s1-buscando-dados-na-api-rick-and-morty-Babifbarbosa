3fd0287b700ccad90bc40f59595fc05fbfb36aa1
babi <babiferreirab15@gmail.com>
2022-02-10 09:44:37 +0100

    comecando

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [characterList, setCharacterList] = useState([]);
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
